61f00c6a4681da03e9652f6c3a00b2f753c41392
Zach Ivester <zivester@gmail.com>
2016-07-31 18:28:55 -0400

    its working

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # reddit-giveaway
+
+
+Usage:
+
+Find all commenters who have '#3' in their post, then select one of them.
+
+```bash
+$ node . https://www.reddit.com/r/hardwareswap/comments/4viicr/meta_giveaway_cooler_master_hyper_212_evo_x2_moto.json '#\s*3'
+```
---
+++ index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+/*
+	Usage: node . [url] [regex]
+ */
+
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'));
+const fs = Promise.promisifyAll(require('fs'));
+
+const url = process.argv[2] || 'https://www.reddit.com/r/hardwareswap/comments/4viicr/meta_giveaway_cooler_master_hyper_212_evo_x2_moto.json';
+const find = process.argv[3] || '#\s*1';
+
+const READ_LOCALLY = false;
+const MUST_HAVE_FLAIR = true;
+
+console.log(`URL:\t ${url}`);
+console.log(`FIND:\t ${find}`);
+if (MUST_HAVE_FLAIR) {
+	console.log('MUST_HAVE_FLAIR');
+}
+
+
+function getJson(url) {
+	return request(url).then(res => {
+		if (res.statusCode !== 200) {
+			throw new Error(`uh oh, something went wrong: ${res.statusCode}`);
+		}
+		return JSON.parse(res.body);
+	});
+}
+
+function readJson(url) {
+	const filename = url.replace(/^.+\/([^/]+)$/, '$1');
+	return fs.readFileAsync(`./${filename}`).then(buf => JSON.parse(buf.toString()));
+}
+
+function findComments(results) {
+	const comments = [];
+	results.forEach(d => {
+		d.data.children.forEach(child => {
+			if (child.kind === 't1') {
+				comments.push(child.data);
+			}
+		});
+	});
+	return comments;
+}
+
+function filterComments(comments) {
+	const entries = {};
+	const regex = new RegExp(find, 'i');
+	comments.forEach(comment => {
+		if (regex.test(comment.body) && (!MUST_HAVE_FLAIR || comment['author_flair_css_class'])) {
+			entries[comment.author] = 1;
+		}
+	});
+	return Object.keys(entries).sort();
+}
+
+function pickOne(users) {
+	return users[Math.floor(Math.random() * users.length)];
+}
+
+
+Promise.resolve()
+	.then(() => {
+		if (READ_LOCALLY) {
+			return readJson(url);
+		}
+		return getJson(url);
+	})
+	.then(findComments)
+	.then(filterComments)
+	.then(entries => {
+		console.log(`${entries.length} valid entries`, entries);
+		return entries;
+	})
+	.then(pickOne)
+	.then(winner => {
+		console.log('Winner', winner);
+		process.exit(0);
+	})
+	.catch(err => {
+		console.error(err);
+		process.exit(1);
+	});
